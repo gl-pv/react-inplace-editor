1c6727555a8aabe11eca31b2d920c1b86a2a11e0
gl.pv <gleeb812812@yandex.ru>
2021-12-13 21:07:16 +0300

    Updates

---
+++ precache-manifest.91e133fc9d42ae1b7ea86d4dc1a0079e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806a2431e9ec2179484cf4e1451a3f10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "756d90dbf97a8de10bf1",
+    "url": "./static/css/main.277f996e.chunk.css"
+  },
+  {
+    "revision": "8329a9758f7f66b966f2",
+    "url": "./static/js/2.cb6be427.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cb6be427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756d90dbf97a8de10bf1",
+    "url": "./static/js/main.75a0ebec.chunk.js"
+  },
+  {
+    "revision": "9bbf296e3b6ef5114d8b",
+    "url": "./static/js/runtime-main.24894d39.js"
+  }
+]);
